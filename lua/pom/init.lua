1dd05835c9747595b18262099107de56e7f2138b
Thomas Casady <tscasady@gmail.com>
2023-02-16 23:18:57 -0600

    Add commands and function skeletons.

---
+++ lua/pom/init.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+M.setup = function (opts)
+end
+
+M.set_timer = function(opts)
+  print 'working?'
+end
+
+M.pause = function(opts)
+  print 'working?'
+end
+
+M.reset = function(opts)
+  print 'working?'
+end
+
+--[[os.time
+os.difftime
+os.clock returns current seconds?, we coudl increment minute every 60 or compare / format os.time or something 
+actually idk what this is doing, it is supposed to be seconds but hte values are werid
+maybe just have a clock count down, idk this should be contnual, but async? 
+what happens to our timer when we close nvim. probably shouldn't do anything
+--]]
+
+
+return M
---
+++ plugin/commands.lua
@@ -0,0 +1,27 @@
+local pom = require('pom')
+--Create Plugin Commands
+local cmd = vim.api.nvim_create_user_command
+
+cmd(
+  'PomTimer',
+  --[[this creates a function, that just calls the next function with opts passed in
+  --opts has built in keys, i just use these
+    args: (string) The args passed to the command, if any <args>
+    count: (number) Any count supplied <count>
+  --]]
+  function(opts) pom.set_timer(opts.args) end,
+  {}
+)
+
+cmd(
+  'Pomreset',
+  function() pom.reset() end,
+  {}
+)
+
+cmd(
+  'Pompause',
+  function() pom.pause() end,
+  {}
+)
+
